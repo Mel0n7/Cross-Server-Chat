1b0ff60642aabdb52cfad239872d5a5b8ede7710
Laiten Kelly <laitenkelly@outlook.com>
2022-03-10 16:54:28 -0500

    Changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord, os, discord.ext, json, requests
 from discord.ext import commands
+from discord.ext.commands import has_permissions
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,23 +16,29 @@ async def on_ready():
 
 
 @client.event
-async def on_message(ctx):
-  if not ctx.author.id == 951564560077320262:
-    print(f"msg in {ctx.guild.name}:{ctx.guild.id} thats says {ctx.content} by {ctx.author}")
-    attachments = ctx.attachments
-    files = []
-    for attachment in attachments:
-      file = await attachment.to_file()
-      files.append(file)
-    with open("./set.json","r") as setF:
-      set = json.loads(setF.read())
-      for server in set:
-        if not server == str(ctx.guild.id):
-          guild = client.get_guild(int(server))
-          channels = guild.text_channels
-          for channel in channels:
-            if set[str(guild.id)] == str(channel.id):
-              await channel.send(f"**{ctx.author}**\n{ctx.content}",files=files)
+async def on_message(message):
+  if not message.content.startswith("c."):
+    with open("./set.json","r") as set:
+      set = json.loads(set.read())
+      if str(message.guild.id) in set:
+        send = set[str(message.guild.id)] == str(message.channel.id)
+      else:
+        send = False
+    if not message.author.id == 951564560077320262 and send:
+      attachments = message.attachments
+      files = []
+      for attachment in attachments:
+        file = await attachment.to_file()
+        files.append(file)
+      with open("./set.json","r") as setF:
+        set = json.loads(setF.read())
+        for server in set:
+          if not server == str(message.guild.id):
+            guild = client.get_guild(int(server))
+            channels = guild.text_channels
+            for channel in channels:
+              if set[str(guild.id)] == str(channel.id):
+                await channel.send(f"**{message.author}**\n{message.content}",files=files)
 
 
 client.remove_command('help')
@@ -41,6 +48,7 @@ async def help(ctx):
 
 
 @client.command(name="set")
+@has_permissions(manage_messages=True)
 async def setChannel(ctx,channel:discord.TextChannel=None):
   if not channel:
     channel = ctx.channel
