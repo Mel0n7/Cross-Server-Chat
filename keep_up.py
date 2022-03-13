5603685066589f6ace9f6f8c987705d13524bce4
mel0n7 <99061680+Mel0n7@users.noreply.github.com>
2022-03-13 20:26:48 +0000

    keep_up update

--- keep_up.py
+++ keep_up.py
@@ -1,9 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
 def home():
-    return "Online"
-  
-def keep_up():
-  app.run(port=80)
+    return "I'm up"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_up():  
+    t = Thread(target=run)
+    t.start()
